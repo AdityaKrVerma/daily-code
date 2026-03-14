1b8c9a0871dc1d5c1b4a8fbe223a05e09dac8bf7
Aditya K Verma <adityakrverma2@gmail.com>
2026-03-13 20:52:05 -0700

    calculator

---
+++ python-basics/day02_calculator.py
@@ -0,0 +1,11 @@
+## Create a file named day02_calculator.py 
+## that asks for two numbers and prints 
+## addition, subtraction, multiplication, and division.
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(f"Addition: {num1 + num2}")
+print(f"Substraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
