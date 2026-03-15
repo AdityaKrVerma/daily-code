48b9b92247b09a8e7e23e162f34ab7da30780d8e
Aditya K Verma <adityakrverma2@gmail.com>
2026-03-15 16:27:33 -0700

    list_practice

---
+++ python-basics/day02_list_practice.py
@@ -0,0 +1,9 @@
+## Create a file named day02_list_practice.py 
+## that stores five incident severities or service names in a list 
+## and prints them one by one.
+
+incidents_and_services = ["Critical Incident", "Service Outage", "System Maintenance", 
+                        "Performance Degradation", "Minor Issues"]
+
+for item in incidents_and_services:
+    print(item)
