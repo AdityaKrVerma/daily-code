cb202ea0cf62c0dbf5b444939c44a9c2281afecd
Aditya K Verma <adityakrverma2@gmail.com>
2026-04-08 18:13:06 -0700

    counter class

---
+++ python-basics/day06_counter_class.py
@@ -0,0 +1,22 @@
+class Counter:
+    def __init__(self, start=0):
+        self.value = start
+
+    def increment(self):
+        self.value += 1
+
+    def decrement(self):
+        self.value -= 1
+
+    def reset(self):
+        self.value = 0
+
+    def get_value(self):
+        return self.value
+
+c = Counter()
+c.increment()
+c.increment()
+print(c.get_value())
+c.reset()
+print(c.get_value())
