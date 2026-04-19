ca24343313e83ab681be46fd76def87bad67fb8b
Aditya K Verma <adityakrverma2@gmail.com>
2026-04-18 21:25:17 -0700

    instance objects as callable

---
+++ python-basics/day06_instance_obj_as_callable.py
@@ -0,0 +1,10 @@
+class Counter:
+    def __init__(self):
+        self.count = 0
+    def __call__(self):
+        self.count += 1
+        return self.count
+
+count = Counter()
+print(count())
+print(count())
