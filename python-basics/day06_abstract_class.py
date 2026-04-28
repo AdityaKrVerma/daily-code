1cf6e737b9c9c7df7cce60b6d3db2a2984a57c7e
Aditya K Verma <adityakrverma2@gmail.com>
2026-04-28 12:00:00 +0530

    using abstract method to define blueprints and force implement of specific methods

---
+++ python-basics/day06_abstract_class.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+    def area(self):  # Must be implemented or Square won't instantiate
+        return self.side * self.side
