e5d47504666c580679141a3ce941bb47eedf74fa
Aditya K Verma <adityakrverma2@gmail.com>
2026-05-17 23:57:47 -0700

    Simple Math, Java!

---
+++ java-basics/day08_SimpleMath.java
@@ -0,0 +1,17 @@
+// Create SimpleMath.java with one class and two methods.
+
+public class SimpleMath {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + add(10, 5));        // Outputs: Sum: 15
+        System.out.println("Difference: " + subtract(10, 5)); // Outputs: Difference: 5
+    }
+}
